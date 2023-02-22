41de3c92375a82da75e57862273316b29457b316
Dev <wandy0394@gmail.com>
2023-02-22 20:00:24 +1100

    added inputs that take RGB values. Coloured squares change colours on input change.

--- palette/src/pages/palette.tsx
+++ palette/src/pages/palette.tsx
@@ -1,13 +1,21 @@
 import ColourPickerSection from "../sections/ColourPickerSection";
+import { HEX } from "../types/colours";
+import {useState} from 'react'
+
+
+
 
 export default function Palette() {
+    const [colours, setColours] = useState<HEX[]>(['000011', '000022', '000033'])
+
+
     return (
         <div className='w-full flex flex-col items-center justify-center'>
             <section className='bg-neutral-800 w-full py-16'>
-                <ColourPickerSection/>
+                <ColourPickerSection colours={colours} setColours={setColours}/>
             </section>
             <section className='bg-neutral-900 w-full py-16'>
-                
+
             </section>
         </div>
     )
--- palette/src/sections/ColourPickerSection.tsx
+++ palette/src/sections/ColourPickerSection.tsx
@@ -1,14 +1,48 @@
-function ColourPicker() {
+import { HEX } from "../types/colours"
+import {useState, ChangeEvent} from "react"
+
+type Props = {
+    colours: HEX[],
+    setColours: React.Dispatch<React.SetStateAction<HEX[]>>
+}
+
+type ColourPickerProps = {
+    colour: HEX,
+    setColour: Function
+}
+
+function ColourPicker(props:ColourPickerProps) {
+    const {colour, setColour} = props
+    // const [colour, setColour] = useState<HEX>(initColour)
+
+    function handleInputChange(e:ChangeEvent<HTMLInputElement>) {
+        e.preventDefault()
+        const value=e.target.value
+        if (value.length > 6) return
+        setColour(e.target.value)
+    }
+
     return (
         <div className='flex flex-col items-center justify-center gap-4'>
-            <div className='rounded bg-neutral-100 aspect-square border border-solid w-full'>
+            <div className={`rounded aspect-square border border-solid w-full hover:border-teal-800 hover:border-2`} style={{backgroundColor: `#${colour}`}}>
+                
             </div>
-            <input className='input'></input>
+            <div className='w-3/4 flex flex-col'>
+                <input className="input w-full text-center" value={`${colour}`} onChange={(e:ChangeEvent<HTMLInputElement>) => handleInputChange(e)}></input>
+                <span className='flex justify-center'>RGB#</span>
+            </div>
         </div>
     )
 }
 
-export default function ColourPickerSection() {
+export default function ColourPickerSection(props:Props) {
+    const {colours, setColours} = props
+
+    function setColour(index:number, colour:HEX) {
+        let newColours = [...colours]
+        newColours[index] = colour
+        setColours(newColours)
+    }
     return (
         <div className='flex flex-col items-center justify-center w-full gap-4'>
             <div className='prose flex flex-col items-center'>
@@ -16,9 +50,11 @@ export default function ColourPickerSection() {
                 <p>We will generate swatches for you</p>
             </div>
             <div className='grid grid-cols-3 w-1/2 gap-4 px-16'>
-                <ColourPicker/>
-                <ColourPicker/>
-                <ColourPicker/>
+                {
+                    colours.map((colour, index)=>{
+                        return <ColourPicker colour={colour} setColour={(colour:HEX)=>setColour(index, colour)}/>
+                    })
+                }
             </div>
             
         </div>
