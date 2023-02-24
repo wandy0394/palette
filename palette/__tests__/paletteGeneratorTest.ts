411e07c4e842371766194868b26bc642e83e81ab
Dev <wandy0394@gmail.com>
2023-02-24 18:07:40 +1100

    Implemented other scheme generators including analogous, square, tetratic, split complementary and unit tested them

--- palette/__tests__/paletteGeneratorTest.ts
+++ palette/__tests__/paletteGeneratorTest.ts
@@ -64,4 +64,156 @@ describe('Testing Palette Generator - Triadic', ()=>{
         let output = palette.getTriadicColourScheme(rgb)
         expect(output).toEqual(['ffffff', 'ffffff', 'ffffff'])
     })
-  })
+    test('Test 6: FF00FF', ()=>{
+        let rgb = 'FF00FF'
+        let output = palette.getTriadicColourScheme(rgb)
+        expect(output).toEqual(['ffff00', 'ff00ff', '00ffff'])
+    })
+    test('Test 7: FFFF00', ()=>{
+        let rgb = 'FFFF00'
+        let output = palette.getTriadicColourScheme(rgb)
+        expect(output).toEqual(['ffff00', 'ff00ff', '00ffff'])
+    })
+    test('Test 8: 00FFFF', ()=>{
+        let rgb = '00FFFF'
+        let output = palette.getTriadicColourScheme(rgb)
+        expect(output).toEqual(['ffff00', 'ff00ff', '00ffff'])
+    })
+})
+
+  describe('Testing Palette Generator - Split Complementary', ()=>{
+    let palette = new PaletteGenerator(new ColourConverter())
+
+    test('Test 1: FF0000', ()=>{
+        let output = palette.getSplitComplementaryScheme('FF0000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ff0000', '00ffbf', '00bfff'], 
+            ['ff8000', 'ff0000', '00bfff'], 
+            ['ff0080', 'ff0000', '00ffbf']
+        ])
+    })
+
+    test('Test 2: 000000', ()=>{
+        let output = palette.getSplitComplementaryScheme('000000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['000000', '000000', '000000'], 
+            ['000000', '000000', '000000'], 
+            ['000000', '000000', '000000']
+        ])
+    })
+
+    test('Test 3: FFFFFF', ()=>{
+        let output = palette.getSplitComplementaryScheme('FFFFFF')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ffffff', 'ffffff', 'ffffff'], 
+            ['ffffff', 'ffffff', 'ffffff'], 
+            ['ffffff', 'ffffff', 'ffffff']
+        ])
+    })
+    test('Test 4: !FFFFF', ()=>{
+        let output = palette.getSplitComplementaryScheme('!FFFFF')
+        expect(output).toEqual(null)
+    })
+})
+
+  describe('Testing Palette Generator - Analogous', ()=>{
+    let palette = new PaletteGenerator(new ColourConverter())
+
+    test('Test 1: FF0000', ()=>{
+        let output = palette.getAnalogousScheme('FF0000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ff4000', 'ff0040', 'ff0000'], 
+            ['ff8000', 'ff4000', 'ff0000'], 
+            ['ff0080', 'ff0040', 'ff0000']
+        ])
+    })
+
+    test('Test 2: 000000', ()=>{
+        let output = palette.getAnalogousScheme('000000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['000000', '000000', '000000'], 
+            ['000000', '000000', '000000'], 
+            ['000000', '000000', '000000']
+        ])
+    })
+
+    test('Test 3: FFFFFF', ()=>{
+        let output = palette.getAnalogousScheme('FFFFFF')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ffffff', 'ffffff', 'ffffff'], 
+            ['ffffff', 'ffffff', 'ffffff'], 
+            ['ffffff', 'ffffff', 'ffffff']
+        ])
+    })
+    test('Test 4: !FFFFF', ()=>{
+        let output = palette.getAnalogousScheme('!FFFFF')
+        expect(output).toEqual(null)
+    })
+})
+
+  describe('Testing Palette Generator - Square', ()=>{
+    let palette = new PaletteGenerator(new ColourConverter())
+
+    test('Test 1: FF0000', ()=>{
+        let output = palette.getSquareScheme('FF0000')
+        expect(output).not.toEqual(null)
+        expect(output).toEqual(['ff0000', '80ff00', '8000ff', '00ffff'])
+    })
+
+    test('Test 2: 000000', ()=>{
+        let output = palette.getSquareScheme('000000')
+        expect(output).not.toEqual(null)
+        expect(output).toEqual(['000000', '000000', '000000', '000000'])
+    })
+
+    test('Test 3: FFFFFF', ()=>{
+        let output = palette.getSquareScheme('FFFFFF')
+        expect(output).not.toEqual(null)
+        expect(output).toEqual(['ffffff', 'ffffff', 'ffffff', 'ffffff'])
+    })
+    test('Test 4: !FFFFF', ()=>{
+        let output = palette.getSquareScheme('!FFFFF')
+        expect(output).toEqual(null)
+    })
+})
+
+  describe('Testing Palette Generator - Tetratic', ()=>{
+    let palette = new PaletteGenerator(new ColourConverter())
+
+    test('Test 1: FF0000', ()=>{
+        let output = palette.getTetraticScheme('FF0000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ff8000', 'ff0000', '00ffff', '0080ff'], 
+            ['ff0080', 'ff0000', '00ffff', '00ff80'], 
+        ])
+    })
+
+    test('Test 2: 000000', ()=>{
+        let output = palette.getTetraticScheme('000000')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['000000', '000000', '000000', '000000'], 
+            ['000000', '000000', '000000', '000000'], 
+        ])
+    })
+
+    test('Test 3: FFFFFF', ()=>{
+        let output = palette.getTetraticScheme('FFFFFF')
+        expect(output).not.toEqual(null)
+        expect(output?.schemes).toEqual([
+            ['ffffff', 'ffffff', 'ffffff', 'ffffff'], 
+            ['ffffff', 'ffffff', 'ffffff', 'ffffff'], 
+        ])
+    })
+    test('Test 4: !FFFFF', ()=>{
+        let output = palette.getTetraticScheme('!FFFFF')
+        expect(output).toEqual(null)
+    })
+})
--- palette/src/model/paletteGenerator.tsx
+++ palette/src/model/paletteGenerator.tsx
@@ -1,6 +1,10 @@
 import { HEX, HSV } from "../types/colours";
 import ColourConverter from "./colourConverter";
 
+type SchemeOutput = {
+    schemes: (HEX[]|null)[]
+        
+} 
 class PaletteGenerator {
     converter:ColourConverter
     constructor(converter:ColourConverter) {
@@ -12,7 +16,26 @@ class PaletteGenerator {
         return n- (m*Math.floor(n/m))
     }
 
-    getComplentaryColourScheme(rgb:HEX):(HEX | null)[] | null {
+    #formatRGBOutput(colourRGB:HEX, ...coloursHSV:HSV[]) : HEX[] | null {
+        const coloursRGB:(HEX | null)[] = coloursHSV.map(colour=>{
+            return this.converter.hsv2rgb(colour)
+        })
+
+        if (coloursRGB.some(elem=>elem === null)) return null
+
+
+        //aim to return colour scheme in ordered format
+        let colours:number[] = [parseInt(colourRGB, 16)]
+        coloursRGB.forEach(colour=>{
+            colours.push(parseInt(colour as string, 16))
+        })
+
+        //by default, Javascript sorts alphabetically, pass comparator function to sort numerically, largest to smallest
+        colours.sort((a,b)=>(b-a))
+
+        return colours.map((colour)=>colour.toString(16).padStart(6,'0'))
+    }
+    getComplentaryColourScheme(rgb:HEX):HEX[] | null {
         const hsv:HSV | null = this.converter.rgb2hsv(rgb)
         if (hsv === null) return null
 
@@ -21,10 +44,12 @@ class PaletteGenerator {
             saturation:hsv.saturation,
             value:hsv.value
         }
-        return [rgb.toLowerCase(), this.converter.hsv2rgb(complementaryHSV)]
+        let output:HEX|null = this.converter.hsv2rgb(complementaryHSV)
+        if (output === null) return null
+        return [rgb.toLowerCase(), output]
     }
 
-    getTriadicColourScheme(rgb:HEX):(HEX | null)[] | null {
+    getTriadicColourScheme(rgb:HEX):HEX[] | null {
         const hsv:HSV | null = this.converter.rgb2hsv(rgb)
         if (hsv === null) return null
 
@@ -34,27 +59,139 @@ class PaletteGenerator {
             value:hsv.value
         }
         
-
         const colour2:HSV = {
             hue: this.#modulo((hsv.hue - 120), 360),
             saturation:hsv.saturation,
             value:hsv.value
         }
         
-        const colour1RGB = this.converter.hsv2rgb(colour1)
-        const colour2RGB = this.converter.hsv2rgb(colour2)
+         return this.#formatRGBOutput(rgb, colour1, colour2)
+    }
 
-        //if invalid colours, return array with nulls, unsorted
-        if (colour1RGB === null || colour2RGB === null) return [rgb.toLowerCase(), colour1RGB,  colour2RGB]
+    getSplitComplementaryScheme(rgb:HEX):SchemeOutput | null {
+        const hsv:HSV | null = this.converter.rgb2hsv(rgb)
+        if (hsv === null) return null
+
+        let output:SchemeOutput = {
+            schemes:[
+                [],
+                [],
+                []
+            ]
+        }
+
+        let angleArray:number[][] = [
+            [165, -165],
+            [30, -165],
+            [-30, 165]
+        ]
+
+        angleArray.forEach((angles, index)=>{
+            let colours:HSV[] = angles.map(angle=>{
+                let colour:HSV = {
+                    hue: this.#modulo((hsv.hue + angle), 360),
+                    saturation:hsv.saturation,
+                    value:hsv.value
+                }
+                return colour
+            })
+
+            output.schemes[index] = this.#formatRGBOutput(rgb, ...colours)
+        })
+
+        return output
+    }
+
+    getAnalogousScheme(rgb:HEX):SchemeOutput | null {
+        const hsv:HSV | null = this.converter.rgb2hsv(rgb)
+        if (hsv === null) return null
+
+        let output:SchemeOutput = {
+            schemes:[
+                [],
+                [],
+                []
+            ]
+        }
+        let angleArray:number[][] = [
+            [15, -15],
+            [30, 15],
+            [-15, -30]
+        ]
+
+        angleArray.forEach((angles, index)=>{
+            let colours:HSV[] = angles.map(angle=>{
+                let colour:HSV = {
+                    hue: this.#modulo((hsv.hue + angle), 360),
+                    saturation:hsv.saturation,
+                    value:hsv.value
+                }
+                return colour
+            })
+
+            output.schemes[index] = this.#formatRGBOutput(rgb, ...colours)
+        })
 
 
-        //otherwise, aim to return colour scheme in ordered format
-        let colours:number[] = [parseInt(rgb, 16), parseInt(colour1RGB as string, 16), parseInt(colour2RGB as string, 16)]
+        return output        
+    }
 
-        //by default, Javascript sorts alphabetically, pass comparator function to sort numerically, largest to smallest
-        colours.sort((a,b)=>(b-a))
+    getSquareScheme(rgb:HEX):HEX[]|null {
+        const hsv:HSV | null = this.converter.rgb2hsv(rgb)
+        if (hsv === null) return null
 
-        return colours.map((colour)=>colour.toString(16).padStart(6,'0'))
+
+        let colour1:HSV = {
+            hue: this.#modulo((hsv.hue + 180), 360),
+            saturation:hsv.saturation,
+            value:hsv.value
+        }
+        let colour2:HSV = {
+            hue: this.#modulo((hsv.hue + 90), 360),
+            saturation:hsv.saturation,
+            value:hsv.value
+        }
+        let colour3:HSV = {
+            hue: this.#modulo((hsv.hue - 90), 360),
+            saturation:hsv.saturation,
+            value:hsv.value
+        }
+        let output = this.#formatRGBOutput(rgb, colour1, colour2, colour3)
+        
+
+        return output              
+    }
+
+    getTetraticScheme(rgb:HEX):SchemeOutput | null {
+        const hsv:HSV | null = this.converter.rgb2hsv(rgb)
+        if (hsv === null) return null
+
+        let output:SchemeOutput = {
+            schemes:[
+                [], 
+                []
+            ]
+        }
+
+
+        let angleArray:number[][] = [
+            [30, 180, 210],
+            [-30, 180, -210],
+        ]
+
+        angleArray.forEach((angles, index)=>{
+            let colours:HSV[] = angles.map(angle=>{
+                let colour:HSV = {
+                    hue: this.#modulo((hsv.hue + angle), 360),
+                    saturation:hsv.saturation,
+                    value:hsv.value
+                }
+                return colour
+            })
+
+            output.schemes[index] = this.#formatRGBOutput(rgb, ...colours)
+        })
+        return output              
     }
 
 }
