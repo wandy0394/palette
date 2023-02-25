e4cffeef786aa942b195bc517d7d0fd322cf8233
Dev <wandy0394@gmail.com>
2023-02-25 21:29:16 +1100

    Random swatch generation implemented for analogous, square and tetratic schemes.

--- palette/src/model/SquareSchemeGenerator.tsx
+++ palette/src/model/SquareSchemeGenerator.tsx
@@ -11,27 +11,53 @@ export default class SquareSchemeGenerator extends PaletteGenerator {
         //generate 10 random colours within the triangle bounded by the square colours
         const colours:SchemeOutput = this.generateScheme(rgb)
 
-        const coloursCartersian:Point[] = colours.schemes[0].map(colour=>{
+        let errorFound:boolean = false
+        const hsvPoints:HSV[] = []
+        colours.schemes[0].forEach(colour=>{
             let hsv:HSV|null = this.converter.rgb2hsv(colour) 
-            if (hsv === null) return {x:NaN, y:NaN}
-            let point:Point = this.hsv2cartesian(hsv)
-            return point
+            if (hsv === null) {
+                errorFound = true
+            }
+            else {
+                hsvPoints.push(hsv)
+            }
+        })
+        if (errorFound) return [[]]
+
+        //sort hsvPoints by hue in increasing order
+        hsvPoints.sort((a,b)=>{
+            return a.hue - b.hue
         })
 
-        let p1:Point = coloursCartersian[0]
-        let p2:Point = coloursCartersian[1]
-        let p3:Point = coloursCartersian[2]
-        let p4:Point = coloursCartersian[3]
+
+        //convert points to cartesian coords
+        const cartPoints:Point[] = hsvPoints.map(hsv=>{
+            return this.hsv2cartesian(hsv)
+        })
+
+        //divide the square into two triangles. The triangle contains the line (hsv[0], hsv[2]) === (cartPoints[0], cartPoints[2])
+        const triangle1:Point[] = [
+            cartPoints[0],
+            cartPoints[1],
+            cartPoints[2]
+        ]
+
+        const triangle2:Point[] = [
+            cartPoints[0],
+            cartPoints[2],
+            cartPoints[3]
+        ]
 
         let temp:HEX[] = []
-        for (let i = 0; i < 10; i++) {
+        //generate random points in triangle1
+        for (let i = 0; i < 5; i++) {
             let r1 = Math.random()
             let r2 = Math.random()
 
             let r1sq = Math.sqrt(r1)
             let randomPoint:Point = {
-                x:(1-r1sq)*p1.x + r1sq*(1-r2)*p2.x+ r2*r1sq*p3.x,
-                y:(1-r1sq)*p1.y + r1sq*(1-r2)*p2.y+ r2*r1sq*p3.y
+                x:(1-r1sq)*triangle1[0].x + r1sq*(1-r2)*triangle1[1].x+ r2*r1sq*triangle1[2].x,
+                y:(1-r1sq)*triangle1[0].y + r1sq*(1-r2)*triangle1[1].y+ r2*r1sq*triangle1[2].y
             } 
 
             let rHSV = this.cartesian2hsv(randomPoint)
@@ -41,10 +67,26 @@ export default class SquareSchemeGenerator extends PaletteGenerator {
             temp.push(rRGB)
         }        
 
-        let output:HEX[][] = [this.sortColoursByHexcode(temp)]
-        // return output
 
-        return []
+        //generate random points in triangle2 and add onto set of points
+        for (let i = 0; i < 5; i++) {
+            let r1 = Math.random()
+            let r2 = Math.random()
+
+            let r1sq = Math.sqrt(r1)
+            let randomPoint:Point = {
+                x:(1-r1sq)*triangle2[0].x + r1sq*(1-r2)*triangle2[1].x+ r2*r1sq*triangle2[2].x,
+                y:(1-r1sq)*triangle2[0].y + r1sq*(1-r2)*triangle2[1].y+ r2*r1sq*triangle2[2].y
+            } 
+
+            let rHSV = this.cartesian2hsv(randomPoint)
+            rHSV.hue = Math.round(rHSV.hue)
+            rHSV.value = Math.random()
+            let rRGB = this.converter.hsv2rgb(rHSV) as HEX
+            temp.push(rRGB)
+        }        
+        let output:HEX[][] = [this.sortColoursByHexcode(temp)]
+        return output
     }
     generateScheme(rgb:HEX):SchemeOutput {
         const hsv:HSV | null = this.converter.rgb2hsv(rgb)
--- palette/src/model/TetraticSchemeGenerator.tsx
+++ palette/src/model/TetraticSchemeGenerator.tsx
@@ -1,3 +1,4 @@
+import { Point } from "../types/cartesian";
 import { HEX, HSV, SchemeOutput } from "../types/colours";
 import ColourConverter from "./colourConverter";
 import PaletteGenerator from "./paletteGenerator";
@@ -7,7 +8,98 @@ export default class TetraticSchemeGenerator extends PaletteGenerator {
         super(converter)
     }
     generateRandomSwatch(rgb: string): HEX[][] {
-        return []
+        //generate 10 random colours within the triangle bounded by the square colours
+        const colours:SchemeOutput = this.generateScheme(rgb)
+
+        let errorFound:boolean = false
+        const hsvPoints:HSV[][] = []
+        colours.schemes.forEach((scheme, index)=>{
+            hsvPoints.push([])
+            scheme.forEach(colour=>{
+                let hsv:HSV|null = this.converter.rgb2hsv(colour) 
+                if (hsv === null) {
+                    errorFound = true
+                }
+                else {
+                    hsvPoints[index].push(hsv)
+                }
+            })
+        })
+        if (errorFound) return [[]]
+
+        //sort hsvPoints by hue in increasing order
+        for (let i = 0; i < hsvPoints.length; i++) {
+            hsvPoints[i].sort((a,b)=>{
+                return a.hue - b.hue
+            })
+        }
+        
+        //convert points to cartesian coords
+        const cartPoints:Point[][] = hsvPoints.map(points=>{
+            return points.map(hsv=>{
+                return this.hsv2cartesian(hsv)
+            })
+        })
+        //divide the square into two triangles. The triangle contains the line (hsv[0], hsv[2]) === (cartPoints[0], cartPoints[2])
+        const triangles:Point[][][] = []
+        for (let i = 0; i < cartPoints.length; i++) {
+            triangles.push([[], []])
+            triangles[i][0] = [
+                cartPoints[i][0],
+                cartPoints[i][1],
+                cartPoints[i][2]
+            ]
+    
+            triangles[i][1] = [
+                cartPoints[i][0],
+                cartPoints[i][2],
+                cartPoints[i][3]
+            ]
+
+        }
+
+        //generate random points within each triangle. All triangles weighted evenly
+        let output:HEX[][] = [[]]
+        for (let i = 0; i < triangles.length; i++) {
+            let temp:HEX[] = []
+            //generate random points in each triangle
+            for (let j = 0; j < 5; j++) {
+                let r1 = Math.random()
+                let r2 = Math.random()
+    
+                let r1sq = Math.sqrt(r1)
+                let randomPoint:Point = {
+                    x:(1-r1sq)*triangles[i][0][0].x + r1sq*(1-r2)*triangles[i][0][1].x+ r2*r1sq*triangles[i][0][2].x,
+                    y:(1-r1sq)*triangles[i][0][0].y + r1sq*(1-r2)*triangles[i][0][1].y+ r2*r1sq*triangles[i][0][2].y
+                } 
+    
+                let rHSV = this.cartesian2hsv(randomPoint)
+                rHSV.hue = Math.round(rHSV.hue)
+                rHSV.value = Math.random()
+                let rRGB = this.converter.hsv2rgb(rHSV) as HEX
+                temp.push(rRGB)
+            }     
+            
+            //generate random points in each triangle
+            for (let j = 0; j < 5; j++) {
+                let r1 = Math.random()
+                let r2 = Math.random()
+    
+                let r1sq = Math.sqrt(r1)
+                let randomPoint:Point = {
+                    x:(1-r1sq)*triangles[i][1][0].x + r1sq*(1-r2)*triangles[i][1][1].x+ r2*r1sq*triangles[i][1][2].x,
+                    y:(1-r1sq)*triangles[i][1][0].y + r1sq*(1-r2)*triangles[i][1][1].y+ r2*r1sq*triangles[i][1][2].y
+                } 
+    
+                let rHSV = this.cartesian2hsv(randomPoint)
+                rHSV.hue = Math.round(rHSV.hue)
+                rHSV.value = Math.random()
+                let rRGB = this.converter.hsv2rgb(rHSV) as HEX
+                temp.push(rRGB)
+            }   
+            output[i] = this.sortColoursByHexcode(temp)
+        }
+        return output
     }
     generateScheme(rgb:HEX):SchemeOutput {
         const hsv:HSV | null = this.converter.rgb2hsv(rgb)
