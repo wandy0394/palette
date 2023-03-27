c273e2dd5010de5bff102c86b540f6ae528328a8
Dev <wandy0394@gmail.com>
2023-03-27 22:02:32 +1100

    user controller unit tests added for signup function

--- library/__tests__/userController.test.tsx
+++ library/__tests__/userController.test.tsx
@@ -136,3 +136,106 @@ describe('Signup Test', ()=>{
     })
     
 })
+
+describe('Login Test', ()=>{
+    var sandbox:any
+    beforeEach(function() {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(function() {
+        sandbox.restore();
+    })
+
+    it('should respond with error, missing email field', async()=>{
+        const mReq:Request = {body:{
+            email:'',
+            password:'',
+        }} as any
+        // const mRes = {
+
+        // }
+        sandbox.stub(UserService, 'login').resolves({
+            id:1,
+            name:'Test',
+            email:'test@test.com'
+        })
+        const mRes:Response = {
+            status: sandbox.stub().returnsThis(),
+            send: sandbox.stub()
+        } as any
+
+        UserController.login(mReq, mRes, ()=>true)
+        await flushPromises()
+        sandbox.assert.calledWith(mRes.status as sinon.SinonSpy, 400)
+        sandbox.assert.calledWith(mRes.send as sinon.SinonSpy, { status: 'error', error: 'Bad request: Missing email field' })
+    })
+
+    it('should respond with error, missing password field', async()=>{
+        const mReq:Request = {body:{
+            email:'test@test.com',
+            password:'',
+        }} as any
+        // const mRes = {
+
+        // }
+        sandbox.stub(UserService, 'login').resolves({
+            id:1,
+            name:'Test',
+            email:'test@test.com'
+        })
+        const mRes:Response = {
+            status: sandbox.stub().returnsThis(),
+            send: sandbox.stub()
+        } as any
+
+        UserController.login(mReq, mRes, ()=>true)
+        await flushPromises()
+        sandbox.assert.calledWith(mRes.status as sinon.SinonSpy, 400)
+        sandbox.assert.calledWith(mRes.send as sinon.SinonSpy, { status: 'error', error: 'Bad request: Missing password field' })
+    })
+
+
+    it('should successfully login', async()=>{
+        const mReq:Request = {body:{
+            email:'test@test.com',
+            password:'password',
+            name:'Test'
+        }} as any
+
+        const mRes:Response = {
+            status: sandbox.stub().returnsThis(),
+            send: sandbox.stub()
+        } as any
+        sandbox.stub(UserService, 'login').resolves({
+            id:1,
+            name:'Test',
+            email:'test@test.com'
+        })
+        UserController.login(mReq, mRes, ()=>true)
+        await flushPromises()
+        sandbox.assert.calledWith(mRes.status as sinon.SinonSpy, 200)
+        sandbox.assert.calledWith(mRes.send as sinon.SinonSpy, { user: { id: 1, name: 'Test', email: 'test@test.com' } })
+    })
+
+    it('should not successfully login', async()=>{
+        const mReq:Request = {body:{
+            email:'test@test.com',
+            password:'password',
+            name:'Test'
+        }} as any
+
+        const mRes:Response = {
+            status: sandbox.stub().returnsThis(),
+            send: sandbox.stub()
+        } as any
+        sandbox.stub(UserService, 'login').rejects({
+            response:'rejected'
+        })
+        UserController.signup(mReq, mRes, ()=>true)
+        await flushPromises()
+        sandbox.assert.calledWith(mRes.status as sinon.SinonSpy, 500)
+        sandbox.assert.calledWith(mRes.send as sinon.SinonSpy, { error:'Internal server error: ' })
+    })
+    
+})
