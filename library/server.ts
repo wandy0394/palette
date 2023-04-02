235145ccd01b61cdedb79e4dd1e96d72207fd1a5
Dev <wandy0394@gmail.com>
2023-04-02 14:16:32 +1000

    added helmet

--- library/server.ts
+++ library/server.ts
@@ -2,10 +2,12 @@ import express, {Request, Express, Response} from "express"
 import cors from "cors"
 import libraryRouter from "./v1/routes/library.routes"
 import userRouter from "./v1/routes/users.routes"
+import helmet from 'helmet'
 
 const app:Express = express()
 app.use(cors())
 app.use(express.json())
+// app.use(helmet)
 app.use("/api/v1/paletteLibrary", libraryRouter)
 app.use("/api/v1/users", userRouter)
 
