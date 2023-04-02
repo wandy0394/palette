282b51ca5786451a304f78711f74cdd2591ef5b8
Dev <wandy0394@gmail.com>
2023-04-02 17:05:06 +1000

    added helmet

--- library/server.ts
+++ library/server.ts
@@ -7,7 +7,7 @@ import helmet from 'helmet'
 const app:Express = express()
 app.use(cors())
 app.use(express.json())
-// app.use(helmet)
+app.use(helmet())
 app.use("/api/v1/paletteLibrary", libraryRouter)
 app.use("/api/v1/users", userRouter)
 
